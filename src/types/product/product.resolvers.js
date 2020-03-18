3b22701cf27943c4c835aeec356287c06dea7022
Elfi Yang <elfiyang16@gmail.com>
2020-03-18 20:06:10 +0000

    lesson 2/3

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -9,10 +9,68 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
+const product = (_, args, ctx) => {
+  if (!ctx.user) {
+    throw new AuthenticationError()
+  }
+  return Product.findById(args.id)
+    .lean()
+    .exec()
+}
+
+const products = (_, args, ctx) => {
+  if (!ctx.user) {
+    throw new AuthenticationError()
+  }
+  return Product.find({})
+    .lean()
+    .exec()
+}
+
+const newProduct = (_, args, ctx) => {
+  if (!ctx.user) {
+    throw new AuthenticationError()
+  }
+  return Product.create({ ...args.input, createdBy: ctx.user._id })
+}
+
+const updateProduct = (_, args, ctx) => {
+  if (!ctx.user || ctx.user.role !== roles.admin) {
+    throw new AuthenticationError()
+  }
+  return Product.findByIdAndUpdate(args.id, args.input, { new: true })
+    .lean()
+    .exec()
+}
+
+const removeProduct = (_, args, ctx) => {
+  if (!ctx.user || ctx.user.role !== roles.admin) {
+    throw new AuthenticationError()
+  }
+  return Product.findByIdAndRemove(args.id)
+    .lean()
+    .exec()
+}
+
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    /* rootresolver so obj is empty */
+    product,
+    products
+  },
+  Mutation: {
+    newProduct,
+    updateProduct,
+    removeProduct
+  },
   Product: {
-    __resolveType(product) {}
+    __resolveType(product) {},
+    createdBy(product) {
+      /* createdBy: User */
+      /* tree, 1st resolve product type => Product type as obj */
+      return User.findById(product.createdBy)
+        .lean()
+        .exec()
+    }
   }
 }
